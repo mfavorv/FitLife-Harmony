837d7bf777984ea1592fa513195d8fcc03d8302d
Favor Vincent <vfavorm77@gmail.com>
2024-06-13 12:02:40 +0300

    get single equipment, search by name and by id complete

--- models/equipment.py
+++ models/equipment.py
@@ -59,3 +59,50 @@ class Equipment:
         cursor.execute(sql, (self.name, self.user_id, self.condition, self.price, self.quantity, self.name))
         conn.commit()
         conn.close()
+
+    @classmethod
+    def single_equipment(cls, row):
+
+        name = row[0]
+        user_id = row[1]
+        condition = row[2]
+        price = row[3]
+        quantity = row[4]
+
+        equipment = cls.all.get(name)
+        if equipment:
+            equipment.name = name
+            equipment.user_id = user_id
+            equipment.condition = condition
+            equipment.price = price
+            equipment.quantity = quantity
+        else:
+            equipment = cls( name ,user_id ,condition ,price ,quantity)
+
+        return equipment
+
+    @classmethod
+    def search_by_id(cls, id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        sql ='''
+            SELECT * 
+            FROM equipment
+            WHERE id is ?
+        '''
+
+        row = cursor.execute(sql, (id,)).fetchone()
+        return cls.single_equipment(row) if row else None
+    
+    @classmethod
+    def search_by_name(cls, name):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        sql ='''
+            SELECT * 
+            FROM equipment 
+            WHERE name is ?
+        '''
+
+        row = cursor.execute(sql, (name,)).fetchone()
+        return cls.single_equipment(row) if row else None
